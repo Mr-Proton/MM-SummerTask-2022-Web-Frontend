d599887c241b7411044b228e7ea20b649d283f09
Mr-Proton <gavynpaulingee@gmail.com>
2022-08-01 00:11:39 +0530

    Integration changes

--- web/src/Components/Home.js
+++ web/src/Components/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import MakeCard from './makeCard'
-import data from './data'
+import React, {useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Home(){
-    var mapOfCards=data.map(items =>(
-        <MakeCard {...items} />
-      ))
+
+    const [data,setData] = useState([])
+
+    useEffect(() =>{
+        fetch('api/article/all').then(res=>res.json).then(result=>console.log(result))
+    },[])
+
+    console.log(data)
+
       
       return(
         <>
@@ -13,8 +19,17 @@ export default function Home(){
                 <h2>Golden Dawn</h2>
                 <h5>Welcome to the official site of Golden Dawn, the reporters of the kingdom of Clover</h5>
             </div>
-            <div className="cards">
-                {mapOfCards}
+            <div className="cards">{
+                data.map(item=>{
+                    <div className="card-body">
+                    <Link to={"localhost:3000/api/article/" + item._id}>
+                        <img className='card-image' src={item.image} alt="" />
+                        <div className="card-texts">
+                            <h3>{item.title}</h3>
+                            <h6>{item.desc}</h6>
+                        </div>
+                    </Link>
+                </div>})}
             </div>
         </>
     )
--- web/src/Components/SignUp.js
+++ web/src/Components/SignUp.js
@@ -1,4 +1,24 @@
+import {useState} from 'react'
+
 export default function SignUp(){
+    const [name,setName] = useState("")
+    const [password,setPassword] = useState("")
+    // const [repassword,setRePassword] = useState("")
+
+    const postData= () =>{
+        fetch("api/userAPI/sign-up",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name:name,
+                password:password
+            })
+        }).then(res=>res.json())
+        .then(data=>console.log(data))
+    }
+    
     return(
         <>
         <div className="heading-2">
@@ -9,19 +29,19 @@ export default function SignUp(){
                 <h1>Sign Up</h1>
                 <form action="">
                     <div className="user-input">
-                        <input className="username" type="text" required/>
+                        <input className="username" value={name} type="text" required onChange={(e)=>setName(e.target.value)}/>
                         <label htmlFor="username">Username</label>
                     </div>
                     <div className="user-input">
-                        <input className="password" type="password" required/>
+                        <input className="password" value={password} type="password" required onChange={(e)=>setPassword(e.target.value)}/>
                         <label htmlFor="password">Password</label>
                     </div>
-                    <div className="user-input">
-                        <input className="re-password" type="password" required/>
+                    {/* <div className="user-input">
+                        <input className="re-password" value={repassword} type="password" required onChange={(e)=>setRePassword(e.target.value)}/>
                         <label htmlFor="re-password">Re-Enter Password</label>
-                    </div>
+                    </div> */}
                     <div className="submit-button">
-                        <button className="submit">Sign Up</button>
+                        <button className="submit" onClick={()=>postData}>Sign Up</button>
                     </div>
                 </form>
             </div>
